1f29be49e3c6e1b70937bf157eb9bab176757999
Lubna1208 <farialubna1208@gmail.com>
2025-08-20 20:40:26 +0600

    Added Profile page and redirect after login

---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+// pages/Profile.js
+import React from "react";
+
+function Profile() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to Your Profile 🎉</h1>
+      <p>Here you’ll find your vaccination details and updates.</p>
+    </div>
+  );
+}
+
+export default Profile;
